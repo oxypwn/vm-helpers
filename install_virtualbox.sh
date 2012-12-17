0c67f10c6bae84981d8b1fff25d83a70b5ba4958
pandrew <letters@paulnotcom.se>
2012-12-17 10:43:46 +0100

    auth needed for repo install

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -4,7 +4,7 @@
 if [ $HOSTNAME = student ]; then
     wget http://download.virtualbox.org/virtualbox/4.2.4/VirtualBox-4.2-4.2.4_81684_el6-1.x86_64.rpm -O /tmp/VirtualBox.rpm
     cd /etc/yum.repos.d
-    wget http://public-yum.oracle.com/public-yum-ol6.repo
+    su -c 'wget http://public-yum.oracle.com/public-yum-ol6.repo'
     su -c 'yum install git'
     cd $HOME && git clone https://github.com/pandrew/vm-helpers.git
 fi
